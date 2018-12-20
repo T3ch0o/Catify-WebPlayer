07c1eb5a568d2c9b92bb8c0a54e739a4e5b7512a
T3ch0o <trottimusbg@gmail.com>
2018-12-20 10:18:29 +0200

    [CatifyDbContext] Added Identity user.

---
+++ WebApi/Catify/Data/CatifyDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Catify.Data
+{
+    using Catify.Entities;
+
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class CatifyDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public CatifyDbContext(DbContextOptions<CatifyDbContext> options) : base(options)
+        {
+        }
+    }
+}
