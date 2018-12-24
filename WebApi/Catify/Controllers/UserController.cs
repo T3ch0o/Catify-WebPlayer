f9064fd64a27db127e4bc7fe96540d183fd43e2a
T3ch0o <trottimusbg@gmail.com>
2018-12-24 17:06:13 +0200

    [UserController] Added Register action.

--- WebApi/Catify/Controllers/UserController.cs
+++ WebApi/Catify/Controllers/UserController.cs
@@ -20,6 +20,7 @@ namespace Catify.Controllers
         }
 
         [HttpPost("login")]
+        [AllowAnonymous]
         [ProducesDefaultResponseType]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> Login([FromBody]LoginBindingModel model)
@@ -40,6 +41,45 @@ namespace Catify.Controllers
             }
 
             return BadRequest(ModelState);
+        }
+
+        [HttpPost("register")]
+        [AllowAnonymous]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Register([FromBody] RegisterBindingModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string token = await _userService.Register(model);
+
+                if (token == null)
+                {
+                    return BadRequest(new { message = "Username already exist." });
+                }
+
+                model.Token = token;
+                model.Password = null;
+                model.RepeatPassword = null;
+
+                return Ok(model);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpGet("logout")]
+        [Authorize]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Logout()
+        {
+            await _userService.Logout();
+
+            return Ok(new
+            {
+                User = ""
+            });
         }
     }
 }
