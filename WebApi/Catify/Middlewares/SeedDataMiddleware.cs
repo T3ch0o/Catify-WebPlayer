7c258deda3687de1a994d02ffe7c637969613368
T3ch0o <trottimusbg@gmail.com>
2018-12-24 17:12:56 +0200

    [SeedDataMiddleware] Creating role Administrator.

---
+++ WebApi/Catify/Middlewares/SeedDataMiddleware.cs
@@ -0,0 +1,29 @@
+﻿namespace Catify.Middlewares
+{
+    using System.Threading.Tasks;
+
+    using Catify.Data;
+
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Identity;
+
+    public class SeedDataMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public SeedDataMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, CatifyDbContext db, RoleManager<IdentityRole> roleManager)
+        {
+            if (!roleManager.RoleExistsAsync("Administrator").Result)
+            {
+                await roleManager.CreateAsync(new IdentityRole("Administrator"));
+            }
+
+            await _next(context);
+        }
+    }
+}
