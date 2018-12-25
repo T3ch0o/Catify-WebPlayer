c13d1747b268234015063c21b62cf302c8ff1431
T3ch0o <trottimusbg@gmail.com>
2018-12-26 00:11:49 +0200

    [PlaylistStatusBindingModel] Added Likes and Favorites properties.

---
+++ WebApi/Catify/Models/BindingModels/PlaylistStatusBindingModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Catify.Models.BindingModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class PlaylistStatusBindingModel
+    {
+        [Range(0, int.MaxValue, ErrorMessage = "Must be positive number.")]
+        public int Likes { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Must be positive number.")]
+        public int Favorites { get; set; }
+    }
+}
