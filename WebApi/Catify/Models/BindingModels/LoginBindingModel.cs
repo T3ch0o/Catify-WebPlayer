0c486cac06a405c74522e7cf9baa47327086ad69
T3ch0o <trottimusbg@gmail.com>
2018-12-25 15:16:16 +0200

    [BindingModels] Changed namespace.

---
+++ WebApi/Catify/Models/BindingModels/LoginBindingModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Catify.Models.BindingModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class LoginBindingModel
+    {
+        [Required] public string Username { get; set; }
+
+        [Required] public string Password { get; set; }
+
+        public string Token { get; set; }
+
+        public string Role { get; set; }
+    }
+}
---
+++ WebApi/Catify/Models/BindingModels/RegisterBindingModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Catify.Models.BindingModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class RegisterBindingModel
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = "Username is too short.")]
+        public string Username { get; set; }
+
+        [Required] [EmailAddress] public string Email { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "Password is too short.")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password), ErrorMessage = "Passwords doesn't match.")]
+        public string RepeatPassword { get; set; }
+
+        public string Token { get; set; }
+
+        public string Role { get; set; }
+    }
+}
