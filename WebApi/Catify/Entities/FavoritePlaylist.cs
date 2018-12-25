8fb03516895dfd64415c476e8d81837c9e146cfe
T3ch0o <trottimusbg@gmail.com>
2018-12-25 13:06:04 +0200

    [FavoritePlaylist] Added Id's of User and Playlist property.

---
+++ WebApi/Catify/Entities/FavoritePlaylist.cs
@@ -0,0 +1,17 @@
+﻿namespace Catify.Entities
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class FavoritePlaylist : BaseEntity<int>
+    {
+        public string UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public CatifyUser User { get; set; }
+
+        public string PlaylistId { get; set; }
+
+        [ForeignKey(nameof(PlaylistId))]
+        public Playlist Playlist { get; set; }
+    }
+}
