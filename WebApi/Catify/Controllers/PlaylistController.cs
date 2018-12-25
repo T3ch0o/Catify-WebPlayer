24d75444009bab760b32e1a965cdec577856690f
T3ch0o <trottimusbg@gmail.com>
2018-12-25 15:10:55 +0200

    [PlaylistController] Added Create action.

--- WebApi/Catify/Controllers/PlaylistController.cs
+++ WebApi/Catify/Controllers/PlaylistController.cs
@@ -1,6 +1,10 @@
 ﻿namespace Catify.Controllers
 {
-    using Catify.BindingModels;
+    using System.Collections.Generic;
+
+    using Catify.Entities;
+    using Catify.Models;
+    using Catify.Models.BindingModels;
     using Catify.Services.Interfaces;
 
     using Microsoft.AspNetCore.Authorization;
@@ -16,23 +20,42 @@
             _playlistService = playlistService;
         }
 
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Get([FromRoute]string id)
+        {
+            return Ok(id);
+        }
+
         [HttpGet("all")]
         [AllowAnonymous]
         [ProducesDefaultResponseType]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult All()
         {
-            return Ok(_playlistService.GetAll());
+            IEnumerable<Playlist> playlists = _playlistService.GetAll();
+            List<AllPlaylistsModel> playlistsModel = new List<AllPlaylistsModel>();
+
+            foreach (Playlist playlist in playlists)
+            {
+                playlistsModel.Add(new AllPlaylistsModel { Creator = playlist.Creator.UserName, Title = playlist.Title, ImageUrl = playlist.ImageUrl });
+            }
+
+            return Ok(playlistsModel);
         }
 
         [HttpPost("create")]
         [Authorize]
         [ProducesDefaultResponseType]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public IActionResult Create(CreatePlaylistBindingModel model)
+        public IActionResult Create([FromBody]CreatePlaylistBindingModel model)
         {
             if (ModelState.IsValid)
             {
+                _playlistService.Create(model, User.Identity.Name);
+
                 return Ok(new
                 {
                     Created = true
