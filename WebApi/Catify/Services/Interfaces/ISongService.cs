1b0fd14314c9d621c1d58f90d7a974d8a5bc6a69
T3ch0o <trottimusbg@gmail.com>
2018-12-25 15:13:04 +0200

    [SongService] Added Create song method.

---
+++ WebApi/Catify/Services/Interfaces/ISongService.cs
@@ -0,0 +1,9 @@
+﻿namespace Catify.Services.Interfaces
+{
+    using Catify.Entities;
+
+    public interface ISongService
+    {
+        void Create(string title, string url, string playlistId);
+    }
+}
---
+++ WebApi/Catify/Services/SongService.cs
@@ -0,0 +1,29 @@
+﻿namespace Catify.Services
+{
+    using Catify.Data;
+    using Catify.Entities;
+    using Catify.Services.Interfaces;
+
+    public class SongService : ISongService
+    {
+        private readonly CatifyDbContext _db;
+
+        public SongService(CatifyDbContext db)
+        {
+            _db = db;
+        }
+
+        public void Create(string title, string url, string playlistId)
+        {
+            Song song = new Song
+            {
+                Title = title,
+                Url = url,
+                PlaylistId = playlistId
+            };
+
+            _db.Songs.Add(song);
+            _db.SaveChanges();
+        }
+    }
+}
