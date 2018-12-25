d922ac978a38919e9f236ce694cd55e077064ce0
T3ch0o <trottimusbg@gmail.com>
2018-12-25 13:05:26 +0200

    [CatifyUser] Added FavoritePlaylists property.

--- WebApi/Catify/Entities/CatifyUser.cs
+++ WebApi/Catify/Entities/CatifyUser.cs
@@ -1,8 +1,11 @@
 ﻿namespace Catify.Entities
 {
+    using System.Collections.Generic;
+
     using Microsoft.AspNetCore.Identity;
 
     public class CatifyUser : IdentityUser
     {
+        public ICollection<FavoritePlaylist> FavoritePlaylists { get; set; } = new HashSet<FavoritePlaylist>();
     }
 }
