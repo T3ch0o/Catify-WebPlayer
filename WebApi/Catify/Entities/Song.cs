8f3c78695fade1a3b60402009750eb669616fd84
T3ch0o <trottimusbg@gmail.com>
2018-12-25 13:06:31 +0200

    [Song] Added properties.

---
+++ WebApi/Catify/Entities/Song.cs
@@ -0,0 +1,16 @@
+﻿namespace Catify.Entities
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Song : BaseEntity<string>
+    {
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public string PlaylistId { get; set; }
+
+        [ForeignKey(nameof(PlaylistId))]
+        public Playlist Playlist { get; set; }
+    }
+}
