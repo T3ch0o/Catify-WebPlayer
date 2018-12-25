825e3ba44e7d57b46f31834108db2bd0bf895f18
T3ch0o <trottimusbg@gmail.com>
2018-12-25 14:02:39 +0200

    [PlaylistController] Added All action to get all playlists.

---
+++ WebApi/Catify/Controllers/PlaylistController.cs
@@ -0,0 +1,45 @@
+﻿namespace Catify.Controllers
+{
+    using Catify.BindingModels;
+    using Catify.Services.Interfaces;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PlaylistController : BaseApiController
+    {
+        private readonly IPlaylistService _playlistService;
+
+        public PlaylistController(IPlaylistService playlistService)
+        {
+            _playlistService = playlistService;
+        }
+
+        [HttpGet("all")]
+        [AllowAnonymous]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult All()
+        {
+            return Ok(_playlistService.GetAll());
+        }
+
+        [HttpPost("create")]
+        [Authorize]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Create(CreatePlaylistBindingModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return Ok(new
+                {
+                    Created = true
+                });
+            }
+
+            return BadRequest(ModelState);
+        }
+    }
+}
