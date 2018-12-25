1259149faf4b00869ea6a73e2943e1c4f265b192
T3ch0o <trottimusbg@gmail.com>
2018-12-25 13:04:28 +0200

    [CatifyDbContext] Added DbSets of playlist entity.

--- WebApi/Catify/Data/CatifyDbContext.cs
+++ WebApi/Catify/Data/CatifyDbContext.cs
@@ -10,5 +10,11 @@
         public CatifyDbContext(DbContextOptions<CatifyDbContext> options) : base(options)
         {
         }
+
+        public DbSet<Playlist> Playlists { get; set; }
+
+        public DbSet<Song> Songs { get; set; }
+
+        public DbSet<FavoritePlaylist> FavoritePlaylists { get; set; }
     }
 }
