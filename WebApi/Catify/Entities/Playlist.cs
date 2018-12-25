56dbbb5c9b9dcde888a63090c0068a004835f947
T3ch0o <trottimusbg@gmail.com>
2018-12-25 13:06:21 +0200

    [Playlist] Added properties.

---
+++ WebApi/Catify/Entities/Playlist.cs
@@ -0,0 +1,21 @@
+﻿namespace Catify.Entities
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Playlist : BaseEntity<string>
+    {
+        public string Title { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int Likes { get; set; }
+
+        public string CreatorId { get; set; }
+
+        [ForeignKey(nameof(CreatorId))]
+        public CatifyUser Creator { get; set; }
+
+        public ICollection<Song> Songs { get; set; } = new HashSet<Song>();
+    }
+}
