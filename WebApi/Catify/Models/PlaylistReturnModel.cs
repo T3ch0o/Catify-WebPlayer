7f090c2d3b1bc17f21e529bfafdfc0bd20a548d4
T3ch0o <trottimusbg@gmail.com>
2018-12-25 15:20:10 +0200

    [PlaylistReturnModel] Added model to take some properties and return them.

---
+++ WebApi/Catify/Models/PlaylistReturnModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Catify.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class PlaylistReturnModel
+    {
+        public string Creator { get; set; }
+
+        public string Title { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string SongUrl { get; set; }
+
+        public string SongTitle { get; set; }
+
+        public int Likes { get; set; }
+
+        public int Favorites { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public ICollection<SongModel> Songs = new HashSet<SongModel>();
+    }
+}
