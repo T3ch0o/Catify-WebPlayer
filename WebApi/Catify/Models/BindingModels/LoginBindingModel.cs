43e27f1a0fb7e61738823fc418a51f16e66f4f46
T3ch0o <trottimusbg@gmail.com>
2018-12-27 23:12:47 +0200

    [LoginBindingModel] Removed Token and Role properties.

--- WebApi/Catify/Models/BindingModels/LoginBindingModel.cs
+++ WebApi/Catify/Models/BindingModels/LoginBindingModel.cs
@@ -7,9 +7,5 @@
         [Required] public string Username { get; set; }
 
         [Required] public string Password { get; set; }
-
-        public string Token { get; set; }
-
-        public string Role { get; set; }
     }
 }
