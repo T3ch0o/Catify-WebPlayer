a66667a2badbce218448168320d90ef3395f25cc
T3ch0o <trottimusbg@gmail.com>
2018-12-29 16:13:19 +0200

    [UserProfileModel] Added.

---
+++ WebApi/Catify/Models/UserProfileModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Catify.Models
+{
+    using System.Collections.Generic;
+
+    public class UserProfileModel
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public ICollection<string> FavoritePlaylists { get; set; }
+
+        public string Role { get; set; }
+    }
+}
