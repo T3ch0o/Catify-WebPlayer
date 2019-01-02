31d922e1b2203ba002c7538b99c8566b9897b6dd
T3ch0o <trottimusbg@gmail.com>
2019-01-03 00:07:29 +0200

    [PlaylistAPI] Added addSong and removeSong to fetch data.

--- Client/src/api/PlaylistAPI.js
+++ Client/src/api/PlaylistAPI.js
@@ -2,19 +2,19 @@ import { appKey } from '../utils/API';
 
 const playlist = {
     get: function(id) {
-        return fetch(`https://baas.kinvey.com/appdata/${appKey}/playlists/${id}`, {
+        return fetch(`https://localhost:44336/api/playlist/${id}`, {
             method: 'GET',
             headers: {
-                Authorization: `Kinvey a4fb937d-eb4e-48c2-8381-08a579ff03cf.0wlBNdB7VuEMh7s+rTLxlV54dHO0mowkXQ5tiOxS9AU=`
+                'Content-Type': 'application/json'
             }
         })
             .then(res => res.json());
     },
     getAll: function() {
-        return fetch(`https://baas.kinvey.com/appdata/${appKey}/playlists`, {
+        return fetch(`https://localhost:44336/api/playlist/all`, {
             method: 'GET',
             headers: {
-                Authorization: `Kinvey a4fb937d-eb4e-48c2-8381-08a579ff03cf.0wlBNdB7VuEMh7s+rTLxlV54dHO0mowkXQ5tiOxS9AU=`
+                Authorization: `Bearer ${localStorage.getItem('authToken')}`
             }
         })
             .then(res => res.json());
@@ -25,21 +25,41 @@ const playlist = {
             .then(res => res.text());
     },
     create: function(payload) {
-        return fetch(`https://baas.kinvey.com/appdata/${appKey}/playlists`, {
+        return fetch(`https://localhost:44336/api/playlist`, {
             method: 'POST',
             headers: {
-                Authorization: `Kinvey ${localStorage.getItem('authToken')}`,
+                Authorization: `Bearer ${localStorage.getItem('authToken')}`,
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(payload)
         })
             .then(res => res.json());
     },
+    addSong: function(payload, id) {
+        return fetch(`https://localhost:44336/api/song/${id}`, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        })
+            .then(res => res.json());
+    },
+    removeSong: function(id) {
+        return fetch(`https://localhost:44336/api/song/${id}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('authToken')}`
+            }
+        })
+            .then(res => res.json());
+    },
     update: function(payload, id) {
-        return fetch(`https://baas.kinvey.com/appdata/${appKey}/playlists/${id}`, {
+        return fetch(`https://localhost:44336/api/playlist/update/${id}`, {
             method: 'PUT',
             headers: {
-                Authorization: `Kinvey 28413438-000a-4776-b865-3dc0135093f0.Q9c5b5i+a4LJ/z/zC5fBnY+NBdsY72mDzwm3JqqfrIk=`,
+                Authorization: `Bearer ${localStorage.getItem('authToken')}`,
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(payload)
@@ -47,10 +67,10 @@ const playlist = {
             .then(res => res.json());
     },
     edit: function(payload, id) {
-        return fetch(`https://baas.kinvey.com/appdata/${appKey}/playlists/${id}`, {
+        return fetch(`https://localhost:44336/api/playlist/${id}`, {
             method: 'PUT',
             headers: {
-                Authorization: `Kinvey ${localStorage.getItem('authToken')}`,
+                Authorization: `Bearer ${localStorage.getItem('authToken')}`,
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(payload)
@@ -58,10 +78,10 @@ const playlist = {
             .then(res => res.json());
     },
     delete: function(id) {
-        return fetch(`https://baas.kinvey.com/appdata/${appKey}/playlists/${id}`, {
+        return fetch(`https://localhost:44336/api/playlist/${id}`, {
             method: 'DELETE',
             headers: {
-                Authorization: `Kinvey ${localStorage.getItem('authToken')}`
+                Authorization: `Bearer ${localStorage.getItem('authToken')}`
             }
         })
             .then(res => res.json());
