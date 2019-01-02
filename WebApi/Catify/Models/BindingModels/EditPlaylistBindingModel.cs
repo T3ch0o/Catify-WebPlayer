e62b452e1cf6e5a3772c04461667a6b005b1e6d6
T3ch0o <trottimusbg@gmail.com>
2019-01-02 23:58:11 +0200

    [EditPlaylistBindingModel] Created.

---
+++ WebApi/Catify/Models/BindingModels/EditPlaylistBindingModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Catify.Models.BindingModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class EditPlaylistBindingModel
+    {
+        [Required(ErrorMessage = "Please enter your playlist title.")]
+        [StringLength(16, MinimumLength = 4, ErrorMessage = "Title must be between 4 and 16 characters.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Please enter image url.")]
+        [RegularExpression(@"(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)", ErrorMessage = "Please enter a valid image url.")]
+        public string ImageUrl { get; set; }
+
+        [RegularExpression(@"(?:\w+,)*\w+", ErrorMessage = "Multiple Tags must be separated with comma.")]
+        public string Tags { get; set; }
+    }
+}
