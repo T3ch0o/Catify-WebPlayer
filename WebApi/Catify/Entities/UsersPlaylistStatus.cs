786367f2322b32dac9ca2c602b747009e8b47f29
T3ch0o <trottimusbg@gmail.com>
2019-01-03 13:32:16 +0200

    [UsersPlaylistStatus] Added status entity for playlist.

---
+++ WebApi/Catify/Entities/UsersPlaylistStatus.cs
@@ -0,0 +1,21 @@
+﻿namespace Catify.Entities
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class UsersPlaylistStatus : BaseEntity<int>
+    {
+        public string UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public CatifyUser User { get; set; }
+
+        public string PlaylistId { get; set; }
+
+        [ForeignKey(nameof(PlaylistId))]
+        public Playlist Playlist { get; set; }
+
+        public bool IsFavorite { get; set; }
+
+        public bool IsLiked { get; set; }
+    }
+}
