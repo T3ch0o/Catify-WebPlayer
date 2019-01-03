58466a23e8fe28f0b1460ce4bc3645d97381ab3c
T3ch0o <trottimusbg@gmail.com>
2019-01-03 13:31:37 +0200

    [CatifyUser] Removed FavoritePlaylist collection property.

--- WebApi/Catify/Entities/CatifyUser.cs
+++ WebApi/Catify/Entities/CatifyUser.cs
@@ -1,11 +1,8 @@
 ﻿namespace Catify.Entities
 {
-    using System.Collections.Generic;
-
     using Microsoft.AspNetCore.Identity;
 
     public class CatifyUser : IdentityUser
     {
-        public ICollection<FavoritePlaylist> FavoritePlaylists { get; set; } = new HashSet<FavoritePlaylist>();
     }
 }
