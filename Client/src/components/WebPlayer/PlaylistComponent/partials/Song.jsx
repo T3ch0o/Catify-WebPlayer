a3071c78da8a0fecb68fc8dddfc2d9d04f002726
T3ch0o <trottimusbg@gmail.com>
2019-01-03 17:01:21 +0200

    [Song] Added choseSong method.

--- Client/src/components/WebPlayer/PlaylistComponent/partials/Song.jsx
+++ Client/src/components/WebPlayer/PlaylistComponent/partials/Song.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const SongsList = function(props) {
-    const { songName, creator ,id, index, songId, deleteSong } = props;
+    const { songName, creator ,id, index, songId, choseSong, deleteSong } = props;
     const isTrue = index === songId;
     const user = localStorage.getItem('user');
     const role = localStorage.getItem('role');
 
     return (
         <li>
-            <Link to={`/web-player/playlist/${id}?song=${index + 1}`} className={isTrue ? 'active' : ''}><i className={isTrue ? `fa fa-play` : `fa fa-music`} aria-hidden="true"/> {songName}</Link>
+            <Link to={`/web-player/playlist/${id}?song=${index + 1}`} className={isTrue ? 'active' : ''}><i className={isTrue ? `fa fa-play` : `fa fa-music`} aria-hidden="true" onClick={() => choseSong()}/> {songName}</Link>
             {creator === user || role === 'Admin' ? <button className="song-delete" onClick={() => deleteSong(songName)}><i className="fa fa-minus" aria-hidden="true"/> </button> : null}
         </li>
     );
