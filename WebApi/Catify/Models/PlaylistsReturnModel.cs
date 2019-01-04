06858a2f8fdd620d6dbdce2a7b03321f3986ef15
T3ch0o <trottimusbg@gmail.com>
2019-01-04 23:42:30 +0200

    [PlaylistsReturnModel] Renamed from AllPlaylistsModel.

---
+++ WebApi/Catify/Models/PlaylistsReturnModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Catify.Models
+{
+    using System;
+
+    public class PlaylistsReturnModel
+    {
+        public string Id { get; set; }
+
+        public string Creator { get; set; }
+
+        public string Title { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public bool IsFavoritePlaylist { get; set; }
+    }
+}
