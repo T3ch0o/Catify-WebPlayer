73121c64f0d9350a6ece77220e411dd065211477
T3ch0o <trottimusbg@gmail.com>
2019-01-04 23:29:39 +0200

    [MappingConfiguration] Created map for models.

---
+++ WebApi/Catify/MappingConfigurations.cs
@@ -0,0 +1,25 @@
+﻿namespace Catify
+{
+    using System;
+
+    using AutoMapper;
+
+    using Catify.Entities;
+    using Catify.Models;
+    using Catify.Models.BindingModels;
+
+    public class MappingConfiguration : Profile
+    {
+        public MappingConfiguration()
+        {
+            CreateMap<RegisterBindingModel, CatifyUser>();
+            CreateMap<PlaylistBindingModel, Playlist>()
+                    .BeforeMap((s, d) => d.CreationDate = DateTime.UtcNow);
+            CreateMap<Song, SongModel>();
+            CreateMap<Playlist, PlaylistReturnModel>()
+                    .ForMember(dest => dest.Creator, m => m.MapFrom(src => src.Creator.UserName));
+            CreateMap<Playlist, PlaylistsReturnModel>()
+                    .ForMember(dest => dest.Creator, m => m.MapFrom(src => src.Creator.UserName));
+        }
+    }
+}
