a85f6a58ab74ea752595caa9baf6463c693ec72c
T3ch0o <trottimusbg@gmail.com>
2019-01-04 23:20:57 +0200

    [PlaylistBindingModel] Removed ImageUrl property.

--- WebApi/Catify/Models/BindingModels/PlaylistBindingModel.cs
+++ WebApi/Catify/Models/BindingModels/PlaylistBindingModel.cs
@@ -8,10 +8,6 @@
         [StringLength(16, MinimumLength = 4, ErrorMessage = "Title must be between 4 and 16 characters.")]
         public string Title { get; set; }
 
-        [Required(ErrorMessage = "Please enter image url.")]
-        [RegularExpression(@"(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)", ErrorMessage = "Please enter a valid image url.")]
-        public string ImageUrl { get; set; }
-
         [Required(ErrorMessage = "Please enter song url.")]
         [RegularExpression(@"((https:\/\/)|(http:\/\/)|(www.)|(m\.)|(\s))+(soundcloud.com\/)+[a-zA-Z0-9\-.]+(\/)+[a-zA-Z0-9\-.]+", ErrorMessage = "Please enter a valid image url.")]
         public string SongUrl { get; set; }
