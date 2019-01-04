3aa3d5424a658963a280b2f22542d0e251826239
T3ch0o <trottimusbg@gmail.com>
2019-01-04 23:32:13 +0200

    [ImageController] Added Upload action to save image.

---
+++ WebApi/Catify/Controllers/ImageController.cs
@@ -0,0 +1,52 @@
+﻿namespace Catify.Controllers
+{
+    using System.IO;
+    using System.Threading.Tasks;
+
+    using Catify.Services.Interfaces;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    using IOFile = System.IO.File;
+
+    public class ImageController : BaseApiController
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        private readonly IPlaylistService _playlistService;
+
+        private readonly string _playlistsImageFolder = "PlaylistImages";
+
+        public ImageController(IHostingEnvironment hostingEnvironment, IPlaylistService playlistService)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            _playlistService = playlistService;
+        }
+
+        [HttpPost("{id}")]
+        [Authorize]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Upload(IFormFile file, string id)
+        {
+            string extension = Path.GetExtension(file.FileName).ToLower();
+
+            if (extension == ".jpeg" || extension == ".png" || extension == ".jpg")
+            {
+                using (FileStream fileStream = IOFile.OpenWrite(Path.Combine(_hostingEnvironment.WebRootPath, _playlistsImageFolder, id + extension)))
+                {
+                    await file.CopyToAsync(fileStream);
+
+                    _playlistService.UpdateImagePath(id, $"{_playlistsImageFolder}/{id + extension}");
+                }
+
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+    }
+}
