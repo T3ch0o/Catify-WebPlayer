9c21a4296304bebdc002413ca7c66f2d87a8a315
T3ch0o <trottimusbg@gmail.com>
2019-01-05 10:29:16 +0200

    [Song] Changed link to Playlist Title.

--- Client/src/components/WebPlayer/PlaylistComponent/partials/Song.jsx
+++ Client/src/components/WebPlayer/PlaylistComponent/partials/Song.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const SongsList = function(props) {
-    const { songName, creator ,id, index, songId, choseSong, deleteSong } = props;
+    const { id, playlistTitle, songName, creator , index, songId, choseSong, deleteSong } = props;
     const isTrue = index === songId;
     const user = localStorage.getItem('user');
     const role = localStorage.getItem('role');
 
     return (
         <li>
-            <Link to={`/web-player/playlist/${id}?song=${index + 1}`} className={isTrue ? 'active' : ''}><i className={isTrue ? `fa fa-play` : `fa fa-music`} aria-hidden="true" onClick={() => choseSong()}/> {songName}</Link>
-            {creator === user || role === 'Admin' ? <button className="song-delete" onClick={() => deleteSong(songName)}><i className="fa fa-minus" aria-hidden="true"/> </button> : null}
+            <Link to={`/web-player/playlist/${playlistTitle.replaceWhiteSpaceWithLine()}?song=${index + 1}`}
+                  className={isTrue ? 'active' : ''}><i className={isTrue ? `fa fa-play` : `fa fa-music`}
+                                                        aria-hidden="true" onClick={() => choseSong()}/> {songName}
+            </Link>
+            {creator === user || role === 'Admin' ? <button className="song-delete" onClick={() => deleteSong(id)}>
+                <i className="fa fa-minus" aria-hidden="true"/> </button> : null}
         </li>
     );
 };
