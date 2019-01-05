a436a93101b0ebfc322254881a89511a4d4d464c
T3ch0o <trottimusbg@gmail.com>
2019-01-05 12:52:14 +0200

    [Startup] Changed Database options to UseNpgsql.

--- WebApi/Catify/Startup.cs
+++ WebApi/Catify/Startup.cs
@@ -32,7 +32,7 @@
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<CatifyDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<CatifyUser, IdentityRole>(options =>
             {
