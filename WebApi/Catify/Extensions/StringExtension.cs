7e2a51cfdedb86fc2233543c42af8d4efcba0e5e
T3ch0o <trottimusbg@gmail.com>
2019-01-05 09:57:52 +0200

    [StringExtension] Added ReplaceLineWithWhitespace method.

---
+++ WebApi/Catify/Extensions/StringExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace Catify.Extensions
+{
+    using System.Text.RegularExpressions;
+
+    public static class StringExtension
+    {
+        public static string ReplaceLineWithWhitespace(this string value)
+        {
+            return Regex.Replace(value, @"\-+", " ");
+        }
+    }
+}
