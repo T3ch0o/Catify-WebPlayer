b6af0059e3886c83ab3ba43095157de75250a3ee
T3ch0o <trottimusbg@gmail.com>
2019-01-05 10:27:17 +0200

    [Playlist] Changed Profile Playlist partial Link tag.

--- Client/src/components/Profile/partials/Playlist.jsx
+++ Client/src/components/Profile/partials/Playlist.jsx
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
 
 const Playlist = function(props) {
-    const { id, image, title, deletePlaylist } = props;
+    const { image, title, deletePlaylist } = props;
 
     const imagePath = `https://localhost:44336/${image}`;
+    const replacedTitle = title.replaceWhiteSpaceWithLine();
 
     return (
         <Fade>
@@ -13,12 +14,12 @@ const Playlist = function(props) {
                 <img src={imagePath} className="image" alt=""/>
                 <div className="middle">
                     <div className="edit-playlist">
-                        <Link to={`/profile/manage-playlists/create/${id}`}><i className="fa fa-plus" aria-hidden="true"/> </Link>
-                        <Link to={`/profile/edit-playlist/${id}`}><i className="fa fa-pencil" aria-hidden="true"/> </Link>
-                        <button onClick={() => deletePlaylist(id)}><i className="fa fa-trash" aria-hidden="true"/> </button>
+                        <Link to={`/profile/manage-playlists/add-song/${replacedTitle}`}><i className="fa fa-plus" aria-hidden="true"/> </Link>
+                        <Link to={`/profile/edit-playlist/${replacedTitle}`}><i className="fa fa-pencil" aria-hidden="true"/> </Link>
+                        <button onClick={() => deletePlaylist(replacedTitle)}><i className="fa fa-trash" aria-hidden="true"/> </button>
                     </div>
                 </div>
-                <Link to={`/web-player/playlist/${id}?song=1`} className="playlist-info">{title}</Link>
+                <Link to={`/web-player/playlist/${replacedTitle}?song=1`} className="playlist-info">{title}</Link>
             </li>
         </Fade>
     );
