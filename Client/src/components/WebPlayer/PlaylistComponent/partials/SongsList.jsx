9c7649601228103d1542944ed36226491d3e7d9c
T3ch0o <trottimusbg@gmail.com>
2019-01-21 16:28:55 +0200

    [SongsList] Add scroll to songs.

--- Client/src/components/WebPlayer/PlaylistComponent/partials/SongsList.jsx
+++ Client/src/components/WebPlayer/PlaylistComponent/partials/SongsList.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import PerfectScrollbar from 'react-perfect-scrollbar'
 
 import Song from './Song';
 
@@ -6,21 +8,23 @@ const SongsList = function(props) {
     const { playlistTitle, songs, creator, id, songId, choseSong, deleteSong } = props;
 
     return (
-        <ul className="songs-list">
-            {songs ? songs.map((s, i) =>
-                <Song
-                    key={i}
-                    index={i}
-                    id={id}
-                    playlistTitle={playlistTitle}
-                    creator={creator}
-                    songId={songId}
-                    songName={s.title}
-                    choseSong={choseSong}
-                    deleteSong={deleteSong}
-                />
-            ) : ''}
-        </ul>
+        <PerfectScrollbar>
+            <ul className="songs-list">
+                {songs ? songs.map((s, i) =>
+                    <Song
+                        key={i}
+                        index={i}
+                        id={id}
+                        playlistTitle={playlistTitle}
+                        creator={creator}
+                        songId={songId}
+                        songName={s.title}
+                        choseSong={choseSong}
+                        deleteSong={deleteSong}
+                    />
+                ) : ''}
+            </ul>
+        </PerfectScrollbar>
     );
 };
 
